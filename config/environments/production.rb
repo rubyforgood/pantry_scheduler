f1f1800dc2cf38332b1c1e064d864968af9c053f
Alexis Johnson <alexis.johnson052@gmail.com>
2017-05-19 10:51:20 -0400

    initial devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # TODO fill in production host information
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
