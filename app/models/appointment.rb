8faee9e5b654d48586aaff5bc9bcebf361084660
Polly Sileo <pollysileo@gmail.com>
2017-05-20 15:20:23 -0400

    Add Model specs to Appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,18 @@
 class Appointment < ApplicationRecord
   belongs_to :client
   has_many :notes, as: :memoable
+
+  APPOINTMENT_TYPES = %w(food utilities).freeze
+
+  validate do |model|
+    if appointment_type.empty? 
+      errors.add :appointment_type, "must have at least one of #{APPOINTMENT_TYPES}"
+    else
+      model.appointment_type.each do |type|
+        unless APPOINTMENT_TYPES.include? type
+          errors.add :appointment_type, "must be one of #{APPOINTMENT_TYPES}"
+        end
+      end
+    end
+  end
 end
---
+++ spec/models/appointment_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+require 'pry'
+
+RSpec.describe Appointment, :type => :model do
+  context "Client is created" do
+    let (:client) { Client.create(first_name: "Paige", last_name: "Bolduc", address: "123 land", county: "Baltimore", zip: 21210, num_adults: 2, num_children: 3, usda_qualifier: true) }
+    let(:default_attrs) { 
+      {
+        time: Date.parse('31-12-2010'),
+        family_size: 4,
+        usda_qualifier: true,
+        appointment_type: ["food"],
+        client_id: client.id,
+      }
+    }
+    it "adds an appointment type successfully" do
+      appointment = Appointment.new(default_attrs)
+      expect(appointment).to be_valid
+    end
+
+    it "fails if using an invalid appointment type" do
+      appointment = Appointment.new(default_attrs.merge(appointment_type: ["invalid_appointment_type"]))
+      expect(appointment).not_to be_valid
+    end
+
+    it "can not have an empty appointment_type array" do
+      appointment = Appointment.new(default_attrs.merge(appointment_type: []))
+      expect(appointment).not_to be_valid
+    end
+  end
+end
