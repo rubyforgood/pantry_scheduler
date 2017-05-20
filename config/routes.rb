e0244fd7f0336614f970c370a37cd06b4ce49071
Paige Bolduc <paigebolduc@gmail.com>
2017-05-20 15:37:49 -0400

    Removed wrapping of JSON in initializer; Added clients index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
 
   namespace :api do
+    resources :clients
     resources :appointments do
       collection do
         get :today
@@ -12,9 +13,4 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   get '*all' => 'home#index'
-
-  namespace :api do
-    resources :clients
-    resources :appointments
-  end
 end
