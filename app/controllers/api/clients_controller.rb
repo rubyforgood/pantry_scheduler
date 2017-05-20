d9dc0228f39bd6b31c8a8a8a1e37dda8bba30bb4
Jamie Gaskins <jgaskins@gmail.com>
2017-05-20 09:51:54 -0400

    Fix clients_controller spec

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -4,14 +4,24 @@ module API
     def create
       client = Client.create!(create_params)
 
-      respond_with client
+      render json: client.as_json
     end
-
 
     private
 
     def create_params
-      params.require(:client).permit(:first_name, :last_name)
+      params
+        .require(:client)
+        .permit(
+          :first_name,
+          :last_name,
+          :address,
+          :county,
+          :zip,
+          :num_adults,
+          :num_children,
+          :usda_qualifier,
+        )
     end
   end
 end
--- spec/requests/client_management_spec.rb
+++ spec/requests/client_management_spec.rb
@@ -1,14 +1,26 @@
 require "rails_helper"
 
 RSpec.describe "Client Managment", type: :request do
+  let(:client_params) do
+    {
+      first_name: "Sally",
+      last_name: "Smith",
+      address: "123 Main St",
+      county: 'Baltimore',
+      zip: '21201',
+      num_adults: 12,
+      num_children: 15,
+      usda_qualifier: false,
+    }
+  end
   it "Creates a Client" do
     expect {
-      post "/api/clients.json", params: { client: {first_name: "Sally", last_name: "Smith"} }
+      post "/api/clients.json", params: { client: client_params }
     }.to change(Client, :count).by(1)
   end
 
   it "Returns Json for the new client" do
-    post "/api/clients.json", params: { client: {first_name: "Sally", last_name: "Smith"} }
+    post "/api/clients.json", params: { client: client_params }
 
     expect(JSON.parse(response.body).fetch("client")).to include("first_name" => "Sally", "last_name" => "Smith")
   end
