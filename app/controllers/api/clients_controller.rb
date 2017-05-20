90fc1074670f2094b503ba3e134f36a0ebcef515
Scott Steele <scott.steele@distilnetworks.com>
2017-05-20 13:34:15 -0400

    Namespace clients controller with a single line

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -1,27 +1,25 @@
-module API
-  class ClientsController < ApplicationController
-    respond_to :json
-    def create
-      client = Client.create!(create_params)
+class API::ClientsController < ApplicationController
+  respond_to :json
+  def create
+    client = Client.create!(create_params)
 
-      render json: client.as_json
-    end
+    render json: client.as_json
+  end
 
-    private
+  private
 
-    def create_params
-      params
-        .require(:client)
-        .permit(
-          :first_name,
-          :last_name,
-          :address,
-          :county,
-          :zip,
-          :num_adults,
-          :num_children,
-          :usda_qualifier,
-        )
-    end
+  def create_params
+    params
+      .require(:client)
+      .permit(
+        :first_name,
+        :last_name,
+        :address,
+        :county,
+        :zip,
+        :num_adults,
+        :num_children,
+        :usda_qualifier,
+      )
   end
 end
