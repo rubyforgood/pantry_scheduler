1a93a2bb0d8187a7efe1b356a4127637dfdee861
Paige Bolduc <paigebolduc@gmail.com>
2017-05-20 16:35:50 -0400

    Replace placeholder data with real clients from API.

--- app/javascript/directory.js
+++ app/javascript/directory.js
@@ -5,64 +5,32 @@ export default class Directory extends React.Component {
     super(props);
 
     this.state = {
-      clients: []
+      clients: [],
     }
   }
 
   componentDidMount() {
-    const clients = [
-      {
-        first_name: "Jane",
-        last_name: "Doe",
-        address: "1234 North Street",
-        county: "Prince George",
-        zip: 32145,
-        phone_number: "4172226666",
-        cell_number: "4173336666",
-        email: "fakeuser@example.com",
-        num_adults: 2,
-        num_children: 4,
-        usda_qualifier: true,
-      },
-      {
-        first_name: "Jane",
-        last_name: "Doe",
-        address: "567 North Street",
-        county: "Prince George",
-        zip: 32145,
-        cell_number: "1232226666",
-        email: "fakeuser2@example.com",
-        usda_cert_date: "2017,01,01",
-        num_adults: 3,
-        num_children: 0,
-        usda_qualifier: true,
-      },
-      {
-        first_name: "Ken",
-        last_name: "Tanaka",
-        address: "40 Sweet Street",
-        county: "Adurundal",
-        zip: 32145,
-        phone_number: "4172226666",
-        email: "fakeuser3@example.com",
-        num_adults: 2,
-        num_children: 4,
-        usda_qualifier: false,
-      },
-    ];
-
-    this.setState({ clients });
+    fetch('/api/clients/')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          clients: json.clients || [],
+       });
+      })
+      .catch(error => {
+        this.setState({ fetchError: error });
+      })
   }
 
   renderClientList() {
-    const clientList = this.state.clients.map((client) => <Client client={client} />);
+    const clientList = this.state.clients.map((client) => <Client client={client} key={client.id} />);
     return <div>{clientList}</div>;
   }
 
   render() {
     return this.state.clients.length > 0
       ? this.renderClientList()
-      : <h4>Loading Clients</h4>
+      : <h4>Directory Unavailable</h4>
   }
 }
 
@@ -83,7 +51,7 @@ const Client = ({ client }) => {
   } = client;
 
   const familySize = `Family Size: ${num_adults + num_children} (Adults: ${num_adults} / Children: ${num_children})`;
-  
+
   const usda = `USDA Status: ${usda_qualifier ? 'yes' : 'no'}
     ${usda_qualifier && usda_cert_date ? 'USDA Certification: ' + usda_cert_date : ''}`;
 
