91275643108b2ef47e5af3c8544f8d4849b73ef8
Paige Bolduc <paigebolduc@gmail.com>
2017-08-12 17:03:28 -0400

    WIP add new note

--- app/javascript/client-form.js
+++ app/javascript/client-form.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { func, object } from 'prop-types';
+import Modal from 'modal';
+import NewNote from 'new-note';
 
 export default class ClientForm extends Component {
   constructor(props) {
@@ -7,6 +9,7 @@ export default class ClientForm extends Component {
 
     this.state = {
       saving: false,
+      showNewNoteModal: false,
     }
   }
 
@@ -68,137 +71,141 @@ export default class ClientForm extends Component {
 
   render() {
     return (
-      <form>
-        <div>
-          <h2>Client Information</h2>
-          <label>Name: </label>
-          <input
-            ref={(element) => this.firstName = element}
-            defaultValue={this.props.client.first_name}
-            disabled={this.state.saving}
-            placeholder="First"
-          />
-          <input
-            ref={(element) => this.lastName = element}
-            defaultValue={this.props.client.last_name}
-            disabled={this.state.saving}
-            placeholder="Last"
-          />
-        </div>
-        <div style={styles.row}>
-          <label>Address: </label>
-          <input
-            ref={(element) => this.address = element}
-            defaultValue={this.props.client.address}
-            disabled={this.state.saving}
-            placeholder="123 Main St"
-          />
-        </div>
-        <div>
-          <label>Zip: </label>
-          <input
-            ref={(element) => this.zip = element}
-            defaultValue={this.props.client.zip}
-            disabled={this.state.saving}
-            placeholder="#####"
-          />
-          <label> County: </label>
-          <select
-            ref={(element) => this.county = element}
-            defaultValue={this.props.client.county}
-            disabled={this.state.saving}
-          >
-            <option />
-            <option value="AA">Anne Arundel</option>
-            <option value="HO">Howard</option>
-            <option value="PG">Prince George</option>
-          </select>
-        </div>
-        <div style={styles.row}>
-          <label style={styles.bold}>Phone </label>
-          <label>(H):</label>
-          <input
-            ref={(element) => this.phoneNumber = element}
-            defaultValue={this.props.client.phone_number}
-            disabled={this.state.saving}
-            placeholder="Home number"
-          />
-          {'  '}
-          <label>(C):</label>
-          <input
-            ref={(element) => this.cellNumber = element}
-            defaultValue={this.props.client.cell_number}
-            disabled={this.state.saving}
-            placeholder="Cell number"
-          />
-        </div>
-        <div>
-          <label>Email: </label>
-          <input
-            ref={(element) => this.email = element}
-            defaultValue={this.props.client.email}
-            disabled={this.state.saving}
-            placeholder="me@example.com"
-          />
-        </div>
-        <div style={styles.row}>
-          <legend style={styles.bold}>Family Info</legend>
-          <label>Adults: </label>
-          <input
-            ref={(element) => this.numAdults = element}
-            defaultValue={this.props.client.num_adults}
-            disabled={this.state.saving}
-          />
-          <label> Children: </label>
-          <input
-            ref={(element) => this.numChildren = element}
-            defaultValue={this.props.client.num_children}
-            disabled={this.state.saving}
-          />
-        </div>
-        <div style={styles.row}>
-          <label>USDA Cert: </label>
-          <label>
+      <div>
+        <form>
+          <div>
+            <h2>Client Information</h2>
+            <label>Name: </label>
             <input
-              name="usda_qualifier"
-              type="radio"
-              defaultChecked={this.props.client.usda_qualifier}
-              onChange={() => this.usdaQualifier = { value: true }}
+              ref={(element) => this.firstName = element}
+              defaultValue={this.props.client.first_name}
+              disabled={this.state.saving}
+              placeholder="First"
             />
-            Y
-          </label>
-          {' '}
-          <label>
             <input
-              name="usda_qualifier"
-              type="radio"
-              ref={() => this.usdaQualifier = { value: this.props.client.usda_qualifier }}
-              defaultChecked={this.props.client.usda_qualifier === false}
-              onChange={() => this.usdaQualifier = { value: false }}
+              ref={(element) => this.lastName = element}
+              defaultValue={this.props.client.last_name}
+              disabled={this.state.saving}
+              placeholder="Last"
             />
-            N
-          </label>
-        </div>
+          </div>
+          <div style={styles.row}>
+            <label>Address: </label>
+            <input
+              ref={(element) => this.address = element}
+              defaultValue={this.props.client.address}
+              disabled={this.state.saving}
+              placeholder="123 Main St"
+            />
+          </div>
+          <div>
+            <label>Zip: </label>
+            <input
+              ref={(element) => this.zip = element}
+              defaultValue={this.props.client.zip}
+              disabled={this.state.saving}
+              placeholder="#####"
+            />
+            <label> County: </label>
+            <select
+              ref={(element) => this.county = element}
+              defaultValue={this.props.client.county}
+              disabled={this.state.saving}
+            >
+              <option />
+              <option value="AA">Anne Arundel</option>
+              <option value="HO">Howard</option>
+              <option value="PG">Prince George</option>
+            </select>
+          </div>
+          <div style={styles.row}>
+            <label style={styles.bold}>Phone </label>
+            <label>(H):</label>
+            <input
+              ref={(element) => this.phoneNumber = element}
+              defaultValue={this.props.client.phone_number}
+              disabled={this.state.saving}
+              placeholder="Home number"
+            />
+            {'  '}
+            <label>(C):</label>
+            <input
+              ref={(element) => this.cellNumber = element}
+              defaultValue={this.props.client.cell_number}
+              disabled={this.state.saving}
+              placeholder="Cell number"
+            />
+          </div>
+          <div>
+            <label>Email: </label>
+            <input
+              ref={(element) => this.email = element}
+              defaultValue={this.props.client.email}
+              disabled={this.state.saving}
+              placeholder="me@example.com"
+            />
+          </div>
+          <div style={styles.row}>
+            <legend style={styles.bold}>Family Info</legend>
+            <label>Adults: </label>
+            <input
+              ref={(element) => this.numAdults = element}
+              defaultValue={this.props.client.num_adults}
+              disabled={this.state.saving}
+            />
+            <label> Children: </label>
+            <input
+              ref={(element) => this.numChildren = element}
+              defaultValue={this.props.client.num_children}
+              disabled={this.state.saving}
+            />
+          </div>
+          <div style={styles.row}>
+            <label>USDA Cert: </label>
+            <label>
+              <input
+                name="usda_qualifier"
+                type="radio"
+                defaultChecked={this.props.client.usda_qualifier}
+                onChange={() => this.usdaQualifier = { value: true }}
+              />
+              Y
+            </label>
+            {' '}
+            <label>
+              <input
+                name="usda_qualifier"
+                type="radio"
+                ref={() => this.usdaQualifier = { value: this.props.client.usda_qualifier }}
+                defaultChecked={this.props.client.usda_qualifier === false}
+                onChange={() => this.usdaQualifier = { value: false }}
+              />
+              N
+            </label>
+          </div>
 
-        <div>
-          { this.props.client.usda_cert_date
-            ? <label>USDA Cert Date: </label>
-            : null
-          }
-        </div>
+          <div>
+            { this.props.client.usda_cert_date
+              ? <label>USDA Cert Date: </label>
+              : null
+            }
+          </div>
 
-        <div style={styles.row}>
-          <label style={styles.bold}>Notes: </label>
-          {this.renderNotesSection()}
-        </div>
+          <div style={styles.row}>
+            <label style={styles.bold}>Notes: </label>
+            {this.renderNotesSection()}
+            <button style={styles.button} onClick={() => {this.setState({showNewNoteModal: true})}}>Add Client Note</button>
+          </div>
 
-        <div>
-          <button style={styles.button} onClick={this.submit.bind(this)}>Save</button>
-          { this.props.onCheckIn && (
-            <button onClick={this.checkIn.bind(this)}>Check In</button>
-          ) }
-        </div>
-      </form>
+          <div>
+            <button style={styles.button} onClick={this.submit.bind(this)}>Save</button>
+            { this.props.onCheckIn && (
+              <button onClick={this.checkIn.bind(this)}>Check In</button>
+            ) }
+          </div>
+        </form>
+        {this.renderNewNoteModal()}
+      </div>
     );
   }
 
@@ -213,6 +220,47 @@ export default class ClientForm extends Component {
      }
      return <div>N/A</div>;
    }
+
+  renderNewNoteModal() {
+    if(this.state.showNewNoteModal) {
+      return (
+        <Modal onClose={() => this.setState({showNewNoteModal: false})}>
+          <div>
+            {
+              this.props.notes.map(note => (
+                <div key={note.id} style={styles.note}>
+                  <p>{note.body}</p>
+                  <button
+                    style={styles.deleteNoteButton}
+                    onClick={() => {
+                      fetch(`/api/clients/${this.props.client.id}/notes/${note.id}`, {
+                        method: 'DELETE',
+                        credentials: 'include',
+                      }).then(() => {
+                        this.setState({
+                          notes: this.props.notes.filter(n => note.id !== n.id)
+                        })
+                      })
+                    }}
+                  >
+                    <img
+                      height='16'
+                      width='16'
+                      src='https://cdn3.iconfinder.com/data/icons/linecons-free-vector-icons-pack/32/trash-512.png'
+                    />
+                  </button>
+                </div>
+              ))
+            }
+            <NewNote
+              key="new-note"
+              clientId={this.props.client.id}
+            />
+          </div>
+        </Modal>
+      );
+    }
+  }
 }
 
 ClientForm.propTypes = {
@@ -234,5 +282,8 @@ const styles = {
   note: {
     margin: '0.25em 0',
     padding: '0 0.5em',
+  },
+  newNoteField: {
+    width: '100%',
   },
 };
